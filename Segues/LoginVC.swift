22bbee794564d32063dbbe47f83e305057538191
wilbhall <joseph.leichter@gmail.com>
2016-12-06 18:46:13 -0500

    Forward and back segue buttons 9_46

---
+++ Segues/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Joseph Leichter on 12/6/16.
+//  Copyright © 2016 Joseph Leichter. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+
+
+}
+
